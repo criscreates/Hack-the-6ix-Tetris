86568739ed3c22a68bc6ab9f1eed10b4e2858ed0
Cristian Moretto <cristianmoretto25@yahoo.ca>
2023-08-19 18:28:39 -0400

    Updated the bag and added hold function

--- src/entities/bag.py
+++ src/entities/bag.py
@@ -1,19 +1,27 @@
 from ..utils.game_config import GameConfig
 from ..utils.constants import PieceType
+from .piece import Piece
 import random
 
 class Bag():
-    def __init__(self, config: GameConfig) -> None:
+    def __init__(self,config: GameConfig) -> None:
+        self.config = config
         self.contents = []
         self.spot = 0
 
-    def refill_bag(self):
-        self.contents = random.shuffle(['T', 'I', 'O', 'S', 'Z', 'J', 'L'])
+    def init_bag(self):
+        return ['T', 'I', 'O', 'S', 'Z', 'J', 'L']
 
+
+    def refill_bag(self):
+        self.contents = self.init_bag()
+        random.shuffle(self.contents)
+    
+    
     def pull_piece(self):
         if self.contents == []:
-            self.contents = self.refill_bag(self)
+            self.refill_bag()
         current = self.contents[0]
         del self.contents[0]
         self.spot += 1
-        return current      
+        return Piece(self.config, current, self.spot)    
--- src/entities/hold.py
+++ src/entities/hold.py
@@ -1,5 +1,33 @@
 from ..utils import GameConfig
+from ..utils.constants import PieceType
+from ..entities.bag import Bag
+import random
 
 class Hold():
     def __init__(self, config: GameConfig) -> None:
-        pass
+        self.held_piece = None
+        self.held_piece_spot = None
+    def hold_piece(self, current_piece, current_piece_spot):
+        if self.held_piece != None:
+            if self.held_piece_spot > current_piece_spot:
+                return current_piece, current_piece_spot
+            else:
+                temp_piece = self.held_piece
+                temp_piece_spot = self.held_piece_spot
+                self.held_piece = current_piece
+                self.held_piece_spot = current_piece_spot
+                current_piece = temp_piece
+                current_piece_spot = temp_piece_spot
+                return current_piece, current_piece_spot
+        else:
+            self.held_piece = current_piece
+            self.held_piece_spot = current_piece_spot
+            return None
+
+
+
+held_piece = Hold()
+# put current piece into hold box
+# if the hold box is full you need to swap
+# put a condition inside the swap function
+# 
