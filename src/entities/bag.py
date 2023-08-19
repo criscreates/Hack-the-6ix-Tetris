f064922842f010e6c0839e6bca508a67156ce9f9
Cristian Moretto <cristianmoretto25@yahoo.ca>
2023-08-19 14:52:57 -0400

    Added a remove and refill function to the Bag

--- src/entities/bag.py
+++ src/entities/bag.py
@@ -1,5 +1,19 @@
-from ..utils import GameConfig
+from ..utils.game_config import GameConfig
+from ..utils.constants import PieceType
+import random
 
 class Bag():
+    contents = PieceType
     def __init__(self, config: GameConfig) -> None:
-        pass
+        self.b = []
+    def refill_bag():
+        contents = random.shuffle(['T', 'I', 'O', 'S', 'Z', 'J', 'L'])
+        return contents
+    def pull_piece(bag_contents):
+        pulled_piece = bag_contents[0]
+        del bag_contents[0]
+        return pulled_piece, bag_contents
+        
+
+# b = Bag()
+# b.refill_bag()
