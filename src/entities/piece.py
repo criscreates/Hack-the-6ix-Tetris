c2ed10345e4ce076e488a93074fcff6558e6d6f4
Starfruit5E <dblin113@gmail.com>
2023-08-19 17:13:00 -0400

    Handle merge conflict for rotation

--- src/entities/piece.py
+++ src/entities/piece.py
@@ -35,41 +35,12 @@ class Piece():
     
 #    def get_positions_vector(self) -> None:
 #        return self.get_positions()
-<<<<<<< HEAD
     def get_Rotated(self, rotation_direction: RotationDirection):
         self.pos4.BODY[1].x
 
     def rotate_math(o_point: Point,rotation_direction: RotationDirection):
+        pass
 
-
-    # def rotate_block(self, rotation_direction: RotationDirection):
-    #     print(tuple(map(lambda x: x.xy(),self.pos4.BODY)))
-    #     orientation = 0
-    #     if (rotation_direction==RotationDirection.CCW):
-    #         self.rotation.go_ccw()
-    #         orientation = self.rotation.get_ccw().rotation
-        
-    #     elif (rotation_direction==RotationDirection.CW):
-    #         self.rotation.go_cw()
-    #         orientation = self.rotation.get_cw().rotation
-        
-    #     print(RotationLookUpTable[self.type][orientation])
-        
-    #     return(RotationLookUpTable[self.type][orientation])
-=======
-    
-    def rotate_block(self, rotation_direction: RotationDirection):
-        # mutate here
-        rotation = self.rotation
-        if (rotation_direction==RotationDirection.CCW):
-            #goleft
-            pass
-        elif (rotation_direction==RotationDirection.CW):
-            #goright
-            pass
-        print(RotationLookUpTable[self.type][rotation_direction])
-        return(RotationLookUpTable[self.type][rotation_direction])
->>>>>>> 85bcc9a30fe84d6983369efc78c7619d1482340f
 
     def get_positions_vector(self) -> tuple[Point, Point, Point, Point]:
         return tuple(map(lambda x: x.xy, self.get_positions()))
