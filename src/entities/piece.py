7d949d53de4ac997a017e13ab9d02c222043cad3
Uangn <nguyenandrew137@protonmail.com>
2023-08-19 17:12:04 -0400

    merge piece rotation conflict

--- src/entities/piece.py
+++ src/entities/piece.py
@@ -1,9 +1,6 @@
 from ..utils import GameConfig
 from ..utils import RotationState
-<<<<<<< HEAD
 from ..utils import PieceType, Point, PIECE_STARTS, POS4, RotationLookUpTable, RotationDirection 
-from math import sin, cos
-=======
 from ..utils import (
     PieceType, 
     Point, 
@@ -14,7 +11,7 @@ from ..utils import (
     BOARD_HEIGHT,
     BOARD_WIDTH,
 ) 
->>>>>>> 85bcc9a30fe84d6983369efc78c7619d1482340f
+from math import sin, cos
 
 class Piece():
     def __init__(self, config: GameConfig, piece_type: PieceType, pos4: POS4 = None) -> None:
@@ -33,31 +30,12 @@ class Piece():
         origin = self.pos4.ORIGIN
         return (origin, *map(lambda v : v.add(origin), self.pos4.BODY))
     
-#    def get_positions_vector(self) -> None:
-#        return self.get_positions()
-<<<<<<< HEAD
     def get_Rotated(self, rotation_direction: RotationDirection):
         self.pos4.BODY[1].x
 
     def rotate_math(o_point: Point,rotation_direction: RotationDirection):
+        pass
 
-
-    # def rotate_block(self, rotation_direction: RotationDirection):
-    #     print(tuple(map(lambda x: x.xy(),self.pos4.BODY)))
-    #     orientation = 0
-    #     if (rotation_direction==RotationDirection.CCW):
-    #         self.rotation.go_ccw()
-    #         orientation = self.rotation.get_ccw().rotation
-        
-    #     elif (rotation_direction==RotationDirection.CW):
-    #         self.rotation.go_cw()
-    #         orientation = self.rotation.get_cw().rotation
-        
-    #     print(RotationLookUpTable[self.type][orientation])
-        
-    #     return(RotationLookUpTable[self.type][orientation])
-=======
-    
     def rotate_block(self, rotation_direction: RotationDirection):
         # mutate here
         rotation = self.rotation
@@ -69,7 +47,6 @@ class Piece():
             pass
         print(RotationLookUpTable[self.type][rotation_direction])
         return(RotationLookUpTable[self.type][rotation_direction])
->>>>>>> 85bcc9a30fe84d6983369efc78c7619d1482340f
 
     def get_positions_vector(self) -> tuple[Point, Point, Point, Point]:
         return tuple(map(lambda x: x.xy, self.get_positions()))
