b399c5fccb5bc6e4db9389f2090d5fe2fc204486
Starfruit5E <dblin113@gmail.com>
2023-08-19 12:42:42 -0400

    piece rotation function made and lookup table started

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -45,3 +45,13 @@ Cell = Enum('Cell', [
     'Placed',
     'None',
 ])
+
+RotationLookUpTable = {
+    PieceType.T: (((0,1),(1,0),(-1,0))),
+    PieceType.I: (((1,0), (1,-1), (0,-1))),
+    PieceType.O: (((1,0), (1,-1), (0,-1))),
+    PieceType.S: (((-1,0), (0,1), (1,1))),
+    PieceType.Z: (((1,0), (0,1), (-1,1))),
+    PieceType.J: (((0,-1), (0,1), (-1,-1))),
+    PieceType.L: (((0,-1), (0,1), (1,-1)))
+}
