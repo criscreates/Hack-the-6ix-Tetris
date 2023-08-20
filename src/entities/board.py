678ee86e751c96e2bde4545f8233ee0f68c797b1
Cristian Moretto <cristianmoretto25@yahoo.ca>
2023-08-19 20:17:23 -0400

    Added a clearing system for the board, and a clear count

--- src/entities/board.py
+++ src/entities/board.py
@@ -10,6 +10,7 @@ class Board():
                  ) -> None:
         self.board = prefill or self.init_board()
         self.piece = piece
+        self.clear_count = 0
     
     def update(self):
         for _ in range(23):
@@ -18,9 +19,21 @@ class Board():
             self.piece.strafe(self, 1)
 
     def init_board(self) -> list[list[Cell]]:
+        self.clear_count = 0
         return [[None for _ in range(10)] for _ in range(20)]
 
+    def clear_check(self, y):
+        return not None in self.board[y]
+    
     def clear_board(self):
+        for y in self.board:
+            if self.clear_check(y) == True:
+                self.clear_count += 1
+                # you could add to the score here! assuming we have time to implement that...
+                self.board[y] = [None for _ in range(10)]
+            
+    def reset_board(self):
+        self.clear_count = 0
         self.board = self.init_board()
         return self
 
